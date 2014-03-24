6f6c3b42aad9432d177f090dc1202b39295747e4
santiavenda2 <santiavenda2@gmail.com>
2014-03-24 18:30:33 -0300

    Agrego funcionalidad para obtener las prmociones de todas las obras

--- plateanet-api.py
+++ plateanet-api.py
@@ -2,9 +2,35 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+from collections import defaultdict
+
+
+def get_obras_en_cartel():
+    """
+    Obtiene todas las obras con sus ids
+    """
+    r = requests.get("https://www.plateanet.com/")
+
+    html = r.text
+
+    soup = BeautifulSoup(html)
+
+    obras_options = soup.select("#Obras > option")
+    obras = {}
+    for obra in obras_options:
+        url_obra = obra.get('value')
+        if url_obra is not None:
+            id_obra = get_obra_id(url_obra)
+            nombre_obra = obra.text
+            obras[id_obra] = (nombre_obra, url_obra)
+
+    return obras
 
 
 def get_initial_info():
+    """
+    Get all the shows and promotions.
+    """
     r = requests.get("https://www.plateanet.com/")
 
     html = r.text
@@ -33,6 +59,9 @@ def get_obra_id(url):
 
 
 def login():
+    """
+    login on www.plateanet.com
+    """
     # you have to complete this to login plateanet user
     data = {
         "IdentityCustomer": "user name",
@@ -44,6 +73,9 @@ def login():
 
 
 def get_info_obra(name):
+    """
+    Usando el nombre (id_name) de la obra obtiene el id_obra y id_teatro
+    """
     obra_url = "https://www.plateanet.com/Obras/"+name
 
     r = requests.get(obra_url)
@@ -62,6 +94,8 @@ def get_info_obra(name):
 
 def get_funciones(id_teatro, id_obra):
     """
+    Usando id_teatro y id_obra obtiene los id_funciones de la obra
+
     var idTeatro=$('#drop1').val()
     idObra=$('#drop1 option:selected').attr('obraId')
     cantidadPedida=parseInt($('#dropEntr').val());
@@ -71,36 +105,70 @@ def get_funciones(id_teatro, id_obra):
     r = requests.post("https://www.plateanet.com/Services/getFuncionesPorTeatroyObra", params=params)
     json_response = r.text
     json_resp = json.loads(json_response)
-    id_funciones = []
+    funciones = {}
     for funcion in json_resp["objeto"]:
-        id_funciones.append(funcion["idFuncion"])
+        id_funcion = funcion["idFuncion"]
+        nombre_funcion = funcion["Nombre"]
+        funciones[id_funcion] = nombre_funcion
 
-    print "id_funciones: ", id_funciones
-    return id_funciones
+    return funciones
 
 
 def get_sectores_y_descuentos(id_funcion):
     """
+    Usando el id de la obra obtiene los sectores y descuentos
     $.post("/Services/getSectoresYDescuentos",{token:"..leofdfojerh.",nIdFuncion:idFuncion}
     """
     params = {"token": "..leofdfojerh.", "nIdFuncion": id_funcion}
     r = requests.post("https://www.plateanet.com/Services/getSectoresYDescuentos", params=params)
     json_response = r.text
     json_resp = json.loads(json_response)
-    print json_resp
+    promociones_encontradas = defaultdict(list)
     for sector in json_resp["objeto"]:
+        totales = int(sector['Totales']) #parseInt(Sectores.objeto[i].Totales)
+        sector_disponible = int(sector['Disponible']) #parseInt(Sectores.objeto[i].Disponible)
+        sector_nombre = sector['Sector']
         for promo in sector["Promos"]:
-            print promo["Nombre"], promo["idAsignacion"], promo["Vendidas"], promo["Quote"]
+            nombre_promo = promo["Nombre"]
+            vendidas = int(promo["Vendidas"]) #parseInt(Sectores.objeto[i].Promos[j].Vendidas);
+            quote = int(promo["Quote"]) #parseInt(Sectores.objeto[i].Promos[j].Quote)
+            tope = min(quote, totales)
+            disp_reales = 0 if tope - vendidas < 0 else tope - vendidas
+            disponibles = min(disp_reales, sector_disponible)
+            if disponibles > 0:
+                promociones_encontradas[nombre_promo].append(sector_nombre)
+                #print "sector: ", sector_nombre, totales, sector_disponible
+                #print nombre_promo, promo["idAsignacion"], vendidas, quote, disponibles, "\n"
+
+    return promociones_encontradas
 
 
 def get_promociones_obra(nombre_obra):
+    print " ------------- OBRA: %s ----------------" %nombre_obra
     id_teatro_w, id_obra_w = get_info_obra(nombre_obra)
     funciones = get_funciones(id_teatro_w, id_obra_w)
-    for funcion in funciones:
-        print "Id funcion:", funcion
-        get_sectores_y_descuentos(funcion)
+    for id_funcion, nombre_funcion in funciones.iteritems():
+        print "Id funcion:", id_funcion, "   nombre funcion: ", nombre_funcion
+        promociones_encontradas = get_sectores_y_descuentos(id_funcion)
+        if promociones_encontradas:
+            for promo, sectores in promociones_encontradas.iteritems():
+                print "promo: ", promo, "   sectores:", sectores
+        else:
+            print "No se encontraron promociones para esta funcion"
 
-get_initial_info()
-#get_promociones_obra("wainraich-y-los-frustrados")
-get_promociones_obra("escenas-de-la-vida-conyugal")
-#login()
+    print "\n"
+
+
+def get_obras_con_promocion():
+    obras = get_obras_en_cartel()
+    for obra_id, obra_info in obras.iteritems():
+        get_promociones_obra(obra_id)
+
+
+if __name__ == "__main__":
+
+    #get_initial_info()
+    #get_promociones_obra("wainraich-y-los-frustrados")
+    #get_promociones_obra("escenas-de-la-vida-conyugal")
+    #login()
+    get_obras_con_promocion()
